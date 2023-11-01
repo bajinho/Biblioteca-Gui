c683f08c46f272b106a79f15f1d367cf822662a2
bajinho <bajo@live.jp>
2023-11-01 06:12:20 -0300

    adicionando pessoas no banco de dados.

--- src/main/java/com/client/bibliotecagui/Application.java
+++ src/main/java/com/client/bibliotecagui/Application.java
@@ -5,10 +5,11 @@
 package com.client.bibliotecagui;
 
 import com.bajo.biblioteca.bean.impl.PessoaRemote;
+import com.bajo.biblioteca.model.Pessoa;
+import com.client.bibliotecagui.invoker.InvokerPessoa;
 import java.security.PrivilegedActionException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.naming.InitialContext;
 import javax.naming.NamingException;
 
 /**
@@ -33,7 +34,8 @@ public class Application extends java.awt.Frame {
     private void initComponents() {
 
         jPanel1 = new javax.swing.JPanel();
-        jButton1 = new javax.swing.JButton();
+        jTextFieldNomedaPessoa = new javax.swing.JTextField();
+        jButtonAdicionarPessoa = new javax.swing.JButton();
         menuBar1 = new java.awt.MenuBar();
         menu1 = new java.awt.Menu();
         menu2 = new java.awt.Menu();
@@ -44,8 +46,16 @@ public class Application extends java.awt.Frame {
             }
         });
 
-        jButton1.setText("jButton1");
-        jPanel1.add(jButton1);
+        jTextFieldNomedaPessoa.setText("Nome da Pessoa");
+        jPanel1.add(jTextFieldNomedaPessoa);
+
+        jButtonAdicionarPessoa.setText("Adicionar Pessoa");
+        jButtonAdicionarPessoa.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButtonAdicionarPessoaActionPerformed(evt);
+            }
+        });
+        jPanel1.add(jButtonAdicionarPessoa);
 
         add(jPanel1, java.awt.BorderLayout.CENTER);
 
@@ -67,6 +77,18 @@ public class Application extends java.awt.Frame {
         System.exit(0);
     }//GEN-LAST:event_exitForm
 
+    private void jButtonAdicionarPessoaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButtonAdicionarPessoaActionPerformed
+        // TODO add your handling code here:
+        PessoaRemote pessoaRemote = InvokerPessoa.invokePessoaStatelessBean();
+        Pessoa p = new Pessoa();
+        p.setNome(jTextFieldNomedaPessoa.getText());
+        try {
+            pessoaRemote.salvar(p);
+        } catch (Exception ex) {
+            Logger.getLogger(Application.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }//GEN-LAST:event_jButtonAdicionarPessoaActionPerformed
+
     /**
      * @param args the command line arguments
      * @throws javax.naming.NamingException
@@ -79,38 +101,14 @@ public class Application extends java.awt.Frame {
             new Application().setVisible(true);
         });
         System.out.println("Hello World!");
-        PessoaRemote pessoaRemote = invokeStatelessBean();
-
-        if (pessoaRemote.consultarPorId(1L) != null) {
-            System.out.println("Pessoa por Id: " + pessoaRemote.consultarPorId(1L).getNome());
-        } else {
-            System.out.println("Deu merda!");
-        }
-
-    }
-
-    /**
-     * Looks up a stateless bean and invokes on it
-     *
-     * @throws NamingException
-     */
-    private static PessoaRemote invokeStatelessBean() {
-        try {
-            InitialContext context = Invoker.getContext();
-            return (PessoaRemote) context.lookup("ejb:/biblioteca-1.0-SNAPSHOT/PessoaBean!"
-                    + PessoaRemote.class.getName());
-        } catch (NamingException e) {
-            System.out.println("ERROR!: " + e);
-            Logger.getLogger(Application.class.getName()).log(Level.SEVERE, null, e);
-        }
-
-        return null;
+       
     }
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButtonAdicionarPessoa;
     private javax.swing.JPanel jPanel1;
+    private javax.swing.JTextField jTextFieldNomedaPessoa;
     private java.awt.Menu menu1;
     private java.awt.Menu menu2;
     private java.awt.MenuBar menuBar1;
