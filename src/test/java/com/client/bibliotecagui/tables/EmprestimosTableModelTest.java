77ede16550a453e6c95ba435f195fcde40ead13c
bajinho <bajinho@pc-temp>
2024-05-19 08:37:38 -0300

    ::update: EmprestimosTableModelTest()

--- src/test/java/com/client/bibliotecagui/tables/EmprestimosTableModelTest.java
+++ src/test/java/com/client/bibliotecagui/tables/EmprestimosTableModelTest.java
@@ -4,141 +4,168 @@
  */
 package com.client.bibliotecagui.tables;
 
+import com.bajo.biblioteca.model.Emprestimo;
+import java.util.ArrayList;
+import java.util.List;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestMethodOrder;
 
 /**
  *
  * @author bajinho
  */
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class EmprestimosTableModelTest {
     
+   private EmprestimosTableModel instance;
+    
+    private final List<Emprestimo> emprestimo = new ArrayList();
+    
+    private final Emprestimo e = new Emprestimo();
+    
+    private final String[] columnNames = new String[]{
+        "ID", "LIVRO", "PESSOA", "DATA EMPRESTIMO", "DATA DEVOLUÇÃO"
+    };
+
     public EmprestimosTableModelTest() {
     }
-    
+
     @BeforeAll
-    public static void setUpClass() {
+    public void setUpClass() {
+        e.setId(1L);
+        e.setPessoa_id(1L);
+        e.setLivro_id(1L);
+        e.setDataemprestimo(null);
+        e.setDatadevolucao(null);
+        emprestimo.add(e);
+        this.instance = new EmprestimosTableModel(emprestimo);
     }
-    
+
     @AfterAll
     public static void tearDownClass() {
     }
-    
+
     @BeforeEach
     public void setUp() {
     }
-    
+
     @AfterEach
     public void tearDown() {
     }
 
     /**
-     * Test of getColumnName method, of class EmprestimosTableModel.
+     * Test of getColumnName method, of class EmprestimosViewTableModel.
      */
     @Test
+    @Order(1)
     public void testGetColumnName() {
         System.out.println("getColumnName");
-        int column = 0;
-        EmprestimosTableModel instance = null;
-        String expResult = "";
-        String result = instance.getColumnName(column);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        for (int x = 0; x <= instance.getColumnCount(); x++) {
+
+            switch (x) {
+                case 0 -> assertEquals(columnNames[x], instance.getColumnName(x));
+                case 1 -> assertEquals(columnNames[x], instance.getColumnName(x));
+                case 2 -> assertEquals(columnNames[x], instance.getColumnName(x));
+                case 3 -> assertEquals(columnNames[x], instance.getColumnName(x));
+                case 4 -> assertEquals(columnNames[x], instance.getColumnName(x));
+                default -> {
+                }
+            }
+        }
     }
 
     /**
-     * Test of getColumnClass method, of class EmprestimosTableModel.
+     * Test of getColumnClass method, of class EmprestimosViewTableModel.
      */
     @Test
+    @Order(2)
     public void testGetColumnClass() {
-        System.out.println("getColumnClass");
-        int columnIndex = 0;
-        EmprestimosTableModel instance = null;
-        Class expResult = null;
-        Class result = instance.getColumnClass(columnIndex);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+       System.out.println("getColumnClass");
+        for (int x = 0; x <= instance.getColumnCount(); x++) {
+            switch (x) {
+                case 0 -> assertEquals(java.lang.Long.class, instance.getColumnClass(x));
+                case 1 -> assertEquals(java.util.Date.class, instance.getColumnClass(x));
+                case 2 -> assertEquals(java.util.Date.class, instance.getColumnClass(x));
+                case 3 -> assertEquals(java.lang.Long.class, instance.getColumnClass(x));
+                case 4 -> assertEquals(java.lang.Long.class, instance.getColumnClass(x));
+                default -> {
+                }
+            }
+        }
     }
 
     /**
      * Test of getColumnCount method, of class EmprestimosTableModel.
      */
     @Test
+    @Order(3)
     public void testGetColumnCount() {
         System.out.println("getColumnCount");
-        EmprestimosTableModel instance = null;
-        int expResult = 0;
+        int expResult = 5;
         int result = instance.getColumnCount();
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
     }
 
     /**
      * Test of getRowCount method, of class EmprestimosTableModel.
      */
     @Test
+    @Order(4)
     public void testGetRowCount() {
         System.out.println("getRowCount");
-        EmprestimosTableModel instance = null;
-        int expResult = 0;
+        int expResult = 1;
         int result = instance.getRowCount();
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
     }
 
     /**
      * Test of getValueAt method, of class EmprestimosTableModel.
      */
     @Test
+    @Order(5)
     public void testGetValueAt() {
         System.out.println("getValueAt");
         int rowIndex = 0;
         int columnIndex = 0;
-        EmprestimosTableModel instance = null;
-        Object expResult = null;
+        Object expResult = 1L;
         Object result = instance.getValueAt(rowIndex, columnIndex);
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
     }
 
     /**
      * Test of isCellEditable method, of class EmprestimosTableModel.
      */
     @Test
+    @Order(6)
     public void testIsCellEditable() {
         System.out.println("isCellEditable");
         int rowIndex = 0;
         int columnIndex = 0;
-        EmprestimosTableModel instance = null;
-        boolean expResult = false;
+        boolean expResult = true;
         boolean result = instance.isCellEditable(rowIndex, columnIndex);
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
     }
 
     /**
      * Test of setValueAt method, of class EmprestimosTableModel.
      */
     @Test
+    @Order(7)
     public void testSetValueAt() {
         System.out.println("setValueAt");
         Object aValue = null;
         int rowIndex = 0;
         int columnIndex = 0;
-        EmprestimosTableModel instance = null;
         instance.setValueAt(aValue, rowIndex, columnIndex);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
     }
     
 }
