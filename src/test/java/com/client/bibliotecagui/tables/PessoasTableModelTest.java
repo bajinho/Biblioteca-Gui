68dfb51eea6ac59993bc30b8fefb023e8eba6123
bajinho <bajinho@pc-temp>
2024-05-19 06:54:42 -0300

    ::update:: PessoasTableModelTest()

--- src/test/java/com/client/bibliotecagui/tables/PessoasTableModelTest.java
+++ src/test/java/com/client/bibliotecagui/tables/PessoasTableModelTest.java
@@ -13,13 +13,17 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestMethodOrder;
 
 /**
  *
  * @author bajinho
  */
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class PessoasTableModelTest {
 
     private PessoasTableModel instance;
@@ -60,23 +64,29 @@ public class PessoasTableModelTest {
      * Test of getColumnName method, of class PessoasTableModel.
      */
     @Test
+    @Order(1)
     public void testGetColumnName() {
         System.out.println("getColumnName");
-        int column = 0;
-        String expResult = columnNames[column];
-        String result = instance.getColumnName(column);
-        assertEquals(expResult, result);
+        for (int x = 0; x <= instance.getColumnCount(); x++) {
+
+            if (0 == x) {
+                assertEquals(columnNames[x], instance.getColumnName(x));
+            } else if (1 == x) {
+                assertEquals(columnNames[x], instance.getColumnName(x));
+            }
+        }
     }
 
     /**
-     * Test of getColumnClass method, of class PessoasTableModel.
-     * for x = columnIndex
+     * Test of getColumnClass method, of class PessoasTableModel. for x =
+     * columnIndex
      */
     @Test
+    @Order(2)
     public void testGetColumnClass() {
         System.out.println("getColumnClass");
         for (int x = 0; x <= instance.getColumnCount(); x++) {
-            
+
             if (0 == x) {
                 assertEquals(java.lang.Long.class, instance.getColumnClass(x));
             } else if (1 == x) {
@@ -89,6 +99,7 @@ public class PessoasTableModelTest {
      * Test of getColumnCount method, of class PessoasTableModel.
      */
     @Test
+    @Order(3)
     public void testGetColumnCount() {
         System.out.println("getColumnCount");
         int expResult = 2;
@@ -100,6 +111,7 @@ public class PessoasTableModelTest {
      * Test of getRowCount method, of class PessoasTableModel.
      */
     @Test
+    @Order(4)
     public void testGetRowCount() {
         System.out.println("getRowCount");
         int expResult = 1;
@@ -111,11 +123,12 @@ public class PessoasTableModelTest {
      * Test of getValueAt method, of class PessoasTableModel.
      */
     @Test
+    @Order(5)
     public void testGetValueAt() {
         System.out.println("getValueAt");
         int rowIndex = 0;
         int columnIndex = 0;
-        Object expResult = null;
+        Object expResult = 100L;
         Object result = instance.getValueAt(rowIndex, columnIndex);
         assertEquals(expResult, result);
     }
@@ -124,6 +137,7 @@ public class PessoasTableModelTest {
      * Test of isCellEditable method, of class PessoasTableModel.
      */
     @Test
+    @Order(6)
     public void testIsCellEditable() {
         System.out.println("isCellEditable");
         int rowIndex = 0;
@@ -137,6 +151,7 @@ public class PessoasTableModelTest {
      * Test of setValueAt method, of class PessoasTableModel.
      */
     @Test
+    @Order(7)
     public void testSetValueAt() {
         System.out.println("setValueAt");
         Object aValue = null;
