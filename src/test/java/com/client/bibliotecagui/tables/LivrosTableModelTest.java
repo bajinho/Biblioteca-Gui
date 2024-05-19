28a591a5dcaf357988811f6dd48c75430fb70259
bajinho <bajinho@pc-temp>
2024-05-19 06:44:22 -0300

    ::update:: LivrosTableModelTest()

--- src/test/java/com/client/bibliotecagui/tables/LivrosTableModelTest.java
+++ src/test/java/com/client/bibliotecagui/tables/LivrosTableModelTest.java
@@ -4,34 +4,58 @@
  */
 package com.client.bibliotecagui.tables;
 
+import com.bajo.biblioteca.model.Livro;
+import java.util.ArrayList;
+import java.util.List;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 
 /**
  *
  * @author bajinho
  */
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@TestMethodOrder(OrderAnnotation.class)
 public class LivrosTableModelTest {
-    
+
+    private LivrosTableModel instance;
+
+    private final List<Livro> livro = new ArrayList();
+
+    private final Livro l = new Livro();
+
+    private final String[] columnNames = new String[]{
+        "ID", "TITULO", "AUTOR", "EMPRESTADO"
+    };
+
     public LivrosTableModelTest() {
     }
-    
+
     @BeforeAll
-    public static void setUpClass() {
+    public void setUpClass() {
+        l.setId(100L);
+        l.setTitulo("LivroTituloTest");
+        l.setAutor("LivroAutorTeste");
+        livro.add(l);
+        this.instance = new LivrosTableModel(livro);
     }
-    
+
     @AfterAll
     public static void tearDownClass() {
     }
-    
+
     @BeforeEach
     public void setUp() {
     }
-    
+
     @AfterEach
     public void tearDown() {
     }
@@ -40,105 +64,105 @@ public class LivrosTableModelTest {
      * Test of getColumnName method, of class LivrosTableModel.
      */
     @Test
+    @Order(1)
     public void testGetColumnName() {
         System.out.println("getColumnName");
-        int column = 0;
-        LivrosTableModel instance = null;
-        String expResult = "";
-        String result = instance.getColumnName(column);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        for (int x = 0; x <= instance.getColumnCount(); x++) {
+
+            switch (x) {
+                case 0 -> assertEquals(columnNames[x], instance.getColumnName(x));
+                case 1 -> assertEquals(columnNames[x], instance.getColumnName(x));
+                case 2 -> assertEquals(columnNames[x], instance.getColumnName(x));
+                case 3 -> assertEquals(columnNames[x], instance.getColumnName(x));
+                default -> {
+                }
+            }
+        }
     }
 
     /**
-     * Test of getColumnClass method, of class LivrosTableModel.
+     * Test of getColumnClass method, of class LivroTableModel. for x =
+     * columnIndex
      */
     @Test
+    @Order(2)
     public void testGetColumnClass() {
         System.out.println("getColumnClass");
-        int columnIndex = 0;
-        LivrosTableModel instance = null;
-        Class expResult = null;
-        Class result = instance.getColumnClass(columnIndex);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        for (int x = 0; x <= instance.getColumnCount(); x++) {
+
+            switch (x) {
+                case 0 -> assertEquals(java.lang.Long.class, instance.getColumnClass(x));
+                case 1 -> assertEquals(java.lang.String.class, instance.getColumnClass(x));
+                case 2 -> assertEquals(java.lang.String.class, instance.getColumnClass(x));
+                case 3 -> assertEquals(java.lang.Boolean.class, instance.getColumnClass(x));
+                default -> {
+                }
+            }
+        }
     }
 
     /**
      * Test of getColumnCount method, of class LivrosTableModel.
      */
     @Test
+    @Order(3)
     public void testGetColumnCount() {
         System.out.println("getColumnCount");
-        LivrosTableModel instance = null;
-        int expResult = 0;
+        int expResult = 4;
         int result = instance.getColumnCount();
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
     }
 
     /**
      * Test of getRowCount method, of class LivrosTableModel.
      */
     @Test
+    @Order(4)
     public void testGetRowCount() {
         System.out.println("getRowCount");
-        LivrosTableModel instance = null;
-        int expResult = 0;
+        int expResult = 1;
         int result = instance.getRowCount();
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
     }
 
     /**
      * Test of getValueAt method, of class LivrosTableModel.
      */
     @Test
+    @Order(5)
     public void testGetValueAt() {
         System.out.println("getValueAt");
         int rowIndex = 0;
         int columnIndex = 0;
-        LivrosTableModel instance = null;
-        Object expResult = null;
+        Object expResult = 100L;
         Object result = instance.getValueAt(rowIndex, columnIndex);
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
     }
 
     /**
      * Test of isCellEditable method, of class LivrosTableModel.
      */
     @Test
+    @Order(6)
     public void testIsCellEditable() {
         System.out.println("isCellEditable");
         int rowIndex = 0;
         int columnIndex = 0;
-        LivrosTableModel instance = null;
-        boolean expResult = false;
+        boolean expResult = true;
         boolean result = instance.isCellEditable(rowIndex, columnIndex);
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
     }
 
     /**
      * Test of setValueAt method, of class LivrosTableModel.
      */
     @Test
+    @Order(7)
     public void testSetValueAt() {
         System.out.println("setValueAt");
         Object aValue = null;
         int rowIndex = 0;
         int columnIndex = 0;
-        LivrosTableModel instance = null;
         instance.setValueAt(aValue, rowIndex, columnIndex);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
     }
-    
 }
